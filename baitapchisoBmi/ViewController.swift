8c1e2865f9fb4222c1c373a58a08ecf1b81daebb
User <user@MacOnes-MacBook-Pro.local>
2020-01-16 14:22:03 +0700

    bai tap 7

---
+++ baitapchisoBmi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  baitapchisoBmi
+//
+//  Created by User on 1/16/20.
+//  Copyright © 2020 hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
